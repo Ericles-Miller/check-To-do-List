66ef34afea27f871f09930c70b43522f3016d376
Ericles-Miller <ericlesmiller15@gmail.com>
2022-12-29 14:05:42 -0300

    Create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-
-
+import toDoListLogo from '../img/Logo.svg';
+import styles from './header.module.css';
 
 export function Header() {
 
-    return (
-        <div></div>
-    );
+  return (
+      <header className={styles.header}>
+        <img
+          className={styles.imgLogo}
+          src={toDoListLogo}
+          alt="logo ToDoList" />
+      </header>
+  );
 }
