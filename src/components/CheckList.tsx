9d25c41db094fbde4b5a9a60b5f9599e37937849
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-02 12:52:59 -0300

    add

--- src/components/CheckList.tsx
+++ src/components/CheckList.tsx
@@ -1,11 +1,12 @@
 import styles from './checkList.module.css';
 import trash from '../img/trash.svg'
+
 export function CheckList() {
   return (
     <div className={styles.UnionAttributesTask}>
       <section>
         <input type="checkbox" id='checkbox' className={styles.checkboxA}/>
-        <label htmlFor='checkbox' className={styles.checkboxLabel}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</label>
+        <label htmlFor='checkbox' className={styles.checkboxLabel}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer sdfsdfsd sdfs ghfghf hegets fsddgsduhrt svdsf s .</label>
         <img src={trash} alt="" />
       </section>
     </div>
