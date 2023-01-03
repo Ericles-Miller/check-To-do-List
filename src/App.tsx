91ba78d15f722df92ff53b57b3330fead7948682
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-03 09:05:38 -0300

    correction css and dad components and son components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react';
 import './global.css';
 import { Header } from './components/Header';
-import styles from './app.module.css';
-import { Tasks } from './components/Tasks';
-import {CreateContent} from './components/CreateContent';
-import { CheckList } from './components/CheckList';
 
+import {CreateContent} from './components/CreateContent';
 
 function App() {
-
   return (
     <div>
-
       <Header></Header>
-      <body className={styles.showWorks} >
+      <body>
         <CreateContent />
-        <Tasks />
-      </body>
-      
-    
+      </body>   
     </div>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,26 +1,39 @@
 import styles from './task.module.css';
 import clipBoard from '../img/Clipboard.svg';
 import { CheckList } from './CheckList';
+import { useState } from 'react';
 
 
-export function Tasks() {
+export function Tasks(props) {
+
+  const [countMakeTask, setCountMakeTask] = useState(0);
+
+  // const [countConclude]
+
+
 	return (
 		<div className={styles.allContentTasks}>
 			<div className={styles.headerTask}>
 				<div className={styles.stylesTask}>
+
 					<p className={styles.newTask}>Tarefas Criadas</p>
 					<span className={styles.number}>0</span>
+
 				</div>
 				<div className={styles.stylesTask}>
+
 					<p className={styles.makeTask}>Concluidas</p>
 					<span className={styles.number}>0</span>
+
 				</div>
 			</div>
 			<div className={styles.showTasks}>
 				<img src={clipBoard} alt="" />
+
 				<p><strong>Você ainda não tem tarefas cadastradas</strong></p>
 				<p>Crie tarefas e organize seus itens a fazer</p>
-				<CheckList></CheckList>
+
+				<CheckList />
 			</div>
 		</div>
 	)
